975ca7e98ad6e8a2c683b76592ea1fc38247938e
cryosx <cyruswu.email@gmail.com>
2018-03-19 21:27:19 -1000

    Finished

--- vault.js
+++ vault.js
@@ -1,4 +1,20 @@
 'use strict';
 module.exports = function() {
+    let vault = {};
+    function setValue(key, value) {
+        vault[key] = value;
+    }
+    function getValue(key) 
+    {
+        if (vault.hasOwnProperty(key)) {
+            return vault[key];
+        } else {
+            return null;
+        }
+    }
 
+  return {
+      setValue: setValue,
+      getValue: getValue,
+  };
 };
